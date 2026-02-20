e0a609cb5f78ba4acc7ae7c4ffa65e4eab404edc
Jan-Øivind Lima <jan.o.lima@ntnu.no>
2026-02-20 15:54:34 +0100

    fix: Re-added rhythm capability with more voices
    Re-added rhythm functionality that was temporarily removed when creating multiple-voice capabilities.

--- code/main.py
+++ code/main.py
@@ -1,22 +1,37 @@
 import music as ms
 import lilyconvert as ly
+import generator as gen
 
 filename = "melody.ly"
 filepath = "code/output/"
 file = filepath + filename
 
 key_name = "eb major"
+time_sig = "4/4"
 key = ms.build_key(key_name)
 
-melody = ms.generate_notes(8, key)
-melody1 = ly.melody_to_lilypond(melody, "voiceOne", "treble")
+rhythm1 = gen.generate_rhythm(4, time_sig, ["q", "h", "q", "e"] )
+rhythm2 = gen.generate_rhythm(4, time_sig, ["q", "h", "q", "e"] )
+rhythm3 = gen.generate_rhythm(4, time_sig, ["q", "h", "q", "e"] )
+rhythm4 = gen.generate_rhythm(4, time_sig, ["q", "h", "q", "e"] )
+melody1 = gen.generate_notes(len(rhythm1), key)
+melody2 = gen.generate_notes(len(rhythm2), key)
+melody3 = gen.generate_notes(len(rhythm3), key)
+melody4 = gen.generate_notes(len(rhythm4), key)
 
-melody = ms.generate_notes(8, key)
-melody2 = ly.melody_to_lilypond(melody, "voiceTwo", "treble")
+part1 = ly.note_and_rhythm(melody1, rhythm1) 
+part2 = ly.note_and_rhythm(melody2, rhythm2) 
+part3 = ly.note_and_rhythm(melody3, rhythm3) 
+part4 = ly.note_and_rhythm(melody4, rhythm4) 
 
-parts = [melody1, melody2]
+melody1 = ly.melody_to_lilypond(part1, "voiceOne", "treble")
+melody2 = ly.melody_to_lilypond(part2, "voiceTwo", "treble")
+melody3 = ly.melody_to_lilypond(part3, "voiceThree", "treble")
+melody4 = ly.melody_to_lilypond(part4, "voiceFour", "treble")
 
-lilycode = ly.voices_to_lilypond(parts, key_name, "4/4")
+parts = [melody1, melody2, melody3, melody4]
+
+lilycode = ly.voices_to_lilypond(parts, key_name, time_sig)
 
 ly.write_to_file(lilycode, file)
 ly.generate_files(file, filepath)
