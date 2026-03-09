923cb908b47ae3e677b070af94370efb998fa41f
Jan-Øivind Lima <jan.o.lima@ntnu.no>
2026-03-09 16:01:28 +0100

    feat(all-files): started refactor, got some working generation, no output files yet
    WIP of creating melodies using better structures

--- code/main.py
+++ code/main.py
@@ -1,1 +1,12 @@
-print("Time to refactor all the code")
+from music import Note, Bar
+import generator as gen
+
+
+number_of_bars = 2
+
+
+for bar in range(number_of_bars):
+    bars = gen.generate_bar(beats=4)
+
+for note in bars.notes:
+    print(note.duration, note.degree, note.rest)
--- code/music.py
+++ code/music.py
@@ -1,7 +1,28 @@
+class Song:
+    def __init__(self, sections):
+        self.sections = sections
+
+class Section:
+    def __init__(self, phrases):
+        self.phrases = phrases
+
+class Phrase:
+    def __init__(self, melodies):
+        self.melodies = melodies
+
 class Melody:
-    def __init__(self, notes):
+    def __init__(self, bars):
+        self.motifs = bars
+
+class Bar:
+    def __init__(self, beats, notes=[]):
+        self.beats = beats
+        self.notes = notes
 
 class Note:
-    def __init__(self, deg):
-
+    def __init__(self, octave, degree, duration, rest=True):
+        self.octave = octave
+        self.degree = degree
+        self.duration = duration
+        self.rest = rest
 
